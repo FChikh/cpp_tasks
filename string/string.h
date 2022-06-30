7bcb8bdfd44cf6fb41de41e54f447028d9f6d0d9
Fedor <fchikhachev@gmail.com>
2022-06-30 12:03:25 +0200

    working GTest on string

--- string/string.h
+++ string/string.h
@@ -6,73 +6,73 @@
 class String
 {
 private:
-    size_t sz = 0;
-    size_t capacity = 0;
-    char *str = nullptr;
+    size_t sz_ = 0;
+    size_t capacity_ = 0;
+    char *str_ = nullptr;
 
     void increaseCapacity(size_t x)
     {
         if (x == 0)
         {
-            capacity = 1;
+            capacity_ = 1;
         }
         else
         {
-            capacity = x * 2;
+            capacity_ = x * 2;
         }
-        char *new_str = new char[capacity];
-        std::copy(str, str + sz, new_str);
-        delete[] str;
-        str = new_str;
+        char *new_str = new char[capacity_];
+        std::copy(str_, str_ + sz_, new_str);
+        delete[] str_;
+        str_ = new_str;
     }
 
     void decreaseCapacity()
     {
-        char *new_str = new char[capacity / 2];
-        std::copy(str, str + sz, new_str);
-        delete[] str;
-        str = new_str;
+        char *new_str = new char[capacity_ / 2];
+        std::copy(str_, str_ + sz_, new_str);
+        delete[] str_;
+        str_ = new_str;
     }
 
 public:
-    String(const char *_str)
+    String(const char *str)
     {
-        sz = strlen(_str);
-        capacity = sz;
-        str = new char[capacity];
-        std::copy(_str, _str + sz, str);
+        sz_ = strlen(str);
+        capacity_ = sz_;
+        str_ = new char[capacity_];
+        std::copy(str, str + sz_, str_);
     }
 
-    String(size_t _sz, char c = '\0')
+    String(size_t sz, char c = '\0')
     {
-        sz = _sz;
-        capacity = sz;
-        str = new char[capacity];
-        memset(str, c, sz);
+        sz_ = sz;
+        capacity_ = sz_;
+        str_ = new char[capacity_];
+        memset(str_, c, sz_);
     }
 
     String(const char c)
     {
-        sz = 1;
-        capacity = sz;
-        str = new char[sz];
-        str[0] = c;
+        sz_ = 1;
+        capacity_ = sz_;
+        str_ = new char[capacity_];
+        str_[0] = c;
     }
 
     String(const String &a)
     {
-        sz = a.sz;
-        capacity = a.capacity;
-        str = new char[capacity];
-        std::copy(a.str, a.str + sz, str);
-    }
-
-    ~String()
-    {
-        delete[] str;
+        sz_ = a.sz_;
+        capacity_ = a.capacity_;
+        str_ = new char[capacity_];
+        std::copy(a.str_, a.str_ + sz_, str_);
     }
 
     String() = default;
+
+    ~String()
+    {
+        delete[] str_;
+    }
 
     String &operator=(String s)
     {
@@ -82,75 +82,75 @@ public:
 
     void swap(String &s)
     {
-        std::swap(s.sz, sz);
-        std::swap(s.capacity, capacity);
-        std::swap(s.str, str);
+        std::swap(s.sz_, sz_);
+        std::swap(s.capacity_, capacity_);
+        std::swap(s.str_, str_);
     }
 
     char &operator[](const size_t i)
     {
-        return str[i];
+        return str_[i];
     }
 
     const char &operator[](const size_t i) const
     {
-        return str[i];
+        return str_[i];
     }
 
     size_t length() const
     {
-        return sz;
+        return sz_;
     }
 
     String substr(const size_t &start, const size_t &count) const
     {
         String ans(count);
-        memcpy(ans.str, str + start, count);
+        memcpy(ans.str_, str_ + start, count);
         return ans;
     }
 
     void clear()
     {
-        delete[] str;
-        str = nullptr;
-        capacity = 0;
-        sz = 0;
+        delete[] str_;
+        str_ = nullptr;
+        capacity_ = 0;
+        sz_ = 0;
     }
 
     bool empty() const
     {
-        return (sz == 0);
+        return (sz_ == 0);
     }
 
     size_t find(const String &t) const
     {
         bool flag = false;
-        size_t find = sz;
-        for (size_t i = 0; i < sz; ++i)
+        size_t find = sz_;
+        for (size_t i = 0; i < sz_; ++i)
         {
-            if (str[i] == t[0])
+            if (str_[i] == t[0])
             {
                 flag = true;
                 find = i;
-                for (size_t sub_i = 0; sub_i < t.sz && i < sz; ++sub_i)
+                for (size_t sub_i = 0; sub_i < t.sz_ && i < sz_; ++sub_i)
                 {
-                    if (t[sub_i] != str[i])
+                    if (t[sub_i] != str_[i])
                     {
                         flag = false;
                         break;
                     }
                     ++i;
                 }
-                if (i - find < t.sz)
+                if (i - find < t.sz_)
                 {
                     flag = false;
                 }
                 if (!flag)
                 {
-                    find = sz;
+                    find = sz_;
                 }
             }
-            if (flag && i - find == t.sz)
+            if (flag && i - find == t.sz_)
             {
                 return find;
             }
@@ -161,24 +161,24 @@ public:
     size_t rfind(const String &t) const
     {
         bool flag = false;
-        size_t find = sz;
-        for (size_t i = 0; i < sz; ++i)
+        size_t find = sz_;
+        for (size_t i = 0; i < sz_; ++i)
         {
-            if (str[i] == t[0])
+            if (str_[i] == t[0])
             {
                 flag = true;
                 size_t cur = i;
                 size_t j = i;
-                for (size_t sub_i = 0; sub_i < t.sz && j < sz; ++sub_i)
+                for (size_t sub_i = 0; sub_i < t.sz_ && j < sz_; ++sub_i)
                 {
-                    if (t[sub_i] != str[j])
+                    if (t[sub_i] != str_[j])
                     {
                         flag = false;
                         break;
                     }
                     ++j;
                 }
-                if (j - cur == t.sz && flag)
+                if (j - cur == t.sz_ && flag)
                 {
                     find = cur;
                 }
@@ -189,32 +189,32 @@ public:
 
     const char &front() const
     {
-        return str[0];
+        return str_[0];
     }
 
     const char &back() const
     {
-        return str[sz - 1];
+        return str_[sz_ - 1];
     }
 
     char &front()
     {
-        return str[0];
+        return str_[0];
     }
 
     char &back()
     {
-        return str[sz - 1];
+        return str_[sz_ - 1];
     }
 
     void pop_back()
     {
-        if (sz == capacity / 4 && sz != 0)
+        if (sz_ == capacity_ / 4 && sz_ != 0)
         {
             decreaseCapacity();
         }
-        str[sz - 1] = '\0';
-        --sz;
+        str_[sz_ - 1] = '\0';
+        --sz_;
     }
 
     void push_back(const char &c)
@@ -224,41 +224,41 @@ public:
 
     String &operator+=(const String &s)
     {
-        if (s.sz + sz >= capacity)
+        if (s.sz_ + sz_ >= capacity_)
         {
-            increaseCapacity(std::max(s.sz, sz));
+            increaseCapacity(std::max(s.sz_, sz_));
         }
-        for (size_t i = 0; i < s.sz; ++i)
+        for (size_t i = 0; i < s.sz_; ++i)
         {
-            str[i + sz] = s[i];
+            str_[i + sz_] = s[i];
         }
-        sz += s.sz;
+        sz_ += s.sz_;
         return *this;
     }
 
     String &operator+=(const char *s)
     {
         size_t len_s = strlen(s);
-        if (sz + len_s >= capacity)
+        if (sz_ + len_s >= capacity_)
         {
-            increaseCapacity(std::max(len_s, sz));
+            increaseCapacity(std::max(len_s, sz_));
         }
         for (size_t i = 0; i < len_s; ++i)
         {
-            str[i + sz] = s[i];
+            str_[i + sz_] = s[i];
         }
-        sz += len_s;
+        sz_ += len_s;
         return *this;
     }
 
     String &operator+=(const char c)
     {
-        if (sz >= capacity)
+        if (sz_ >= capacity_)
         {
-            increaseCapacity(capacity);
+            increaseCapacity(capacity_);
         }
-        str[sz] = c;
-        ++sz;
+        str_[sz_] = c;
+        ++sz_;
         return *this;
     }
 };
@@ -278,8 +278,7 @@ bool operator==(const String &a, const String &b)
     }
     else
     {
-        size_t i = 0;
-        for (; i < a.length(); ++i)
+        for (size_t i = 0; i < a.length(); ++i)
         {
             if (a[i] != b[i])
             {
@@ -288,6 +287,44 @@ bool operator==(const String &a, const String &b)
         }
         return true;
     }
+}
+
+bool operator<(const String &a, const String &b)
+{
+    for (size_t i = 0; i < std::min(a.length(), b.length()); ++i)
+    {
+        if (a[i] < b[i])
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    if (a.length() < b.length())
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool operator>(const String &a, const String &b)
+{
+    return (b < a);
+}
+
+bool operator<=(const String &a, const String &b)
+{
+    return (a < b || a == b);
+}
+
+bool operator>=(const String &a, const String &b)
+{
+    return (a > b || a == b);
 }
 
 std::ostream &operator<<(std::ostream &out, const String &s)
